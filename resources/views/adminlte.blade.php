3b3a8e0d22da2c539a7e31a9e32234a1260e4bf0
Roger Forner Fabre <hello@rogerforner.com>
2017-04-04 17:55:53 +0200

    Arreglem el layout acord amb t.bootstrap

--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -7,22 +7,28 @@
 
 @section('main-content')
 
-    <wizard>
-        <wizard-step title="Usuari" active="true">
-            Step 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et minus quia quis totam veritatis? Animi aut blanditiis esse recusandae velit voluptatem. Consectetur corporis dolor hic maxime nam officiis perferendis, reiciendis.
-        </wizard-step>
-        <wizard-step title="Dades personals">
-            Step 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores distinctio error expedita ipsa laboriosam libero nulla reprehenderit tenetur. Accusantium animi et fuga itaque iure nam numquam quia sapiente, tempora voluptatum?
-        </wizard-step>
-        <wizard-step title="Estudi i curs">
-            Step 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda incidunt iure molestiae mollitia quas repellendus sapiente vel! Aperiam cumque eius facere incidunt maiores nihil nisi provident repellendus sequi voluptatibus!
-        </wizard-step>
-        <wizard-step title="Mòduls professionals">
-            Step 4. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab autem consequuntur dolorum numquam odio quasi qui quisquam quod soluta? At dicta dignissimos doloremque fuga laboriosam numquam perferendis placeat sint velit.
-        </wizard-step>
-        <wizard-step title="Unitats formatives">
-            Step 5. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid aspernatur at dolore, dolores esse in officiis quibusdam quis tempore, temporibus vel. Accusamus ad amet dicta hic, impedit itaque temporibus.
-        </wizard-step>
-    </wizard>
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-12">
+                <wizard>
+                    <wizard-step title="Usuari" active="true">
+                        Usuari. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </wizard-step>
+                    <wizard-step title="Dades personals">
+                        Dades personals. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </wizard-step>
+                    <wizard-step title="Estudi i curs">
+                        Estudi i curs. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </wizard-step>
+                    <wizard-step title="Mòduls professionals">
+                        Mòduls professionals. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </wizard-step>
+                    <wizard-step title="Unitats formatives">
+                        Unitats formatives. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </wizard-step>
+                </wizard>
+            </div>
+        </div>
+    </div>
 
 @endsection
